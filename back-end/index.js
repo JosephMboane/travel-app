3ee0d150e5aae547953dbc89a9a5adedbb4442fe
JosephMboane <31317713+JosephMboane@users.noreply.github.com>
2023-08-03 10:48:14 +0200

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -25,14 +25,14 @@ app.get('/country', (req, res) => {
 
 
     //temperatura api
-    // const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
+    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
 
     //Cambios api
     // const apiUrl = 'http://api.exchangeratesapi.io/v1/symbols?access_key=f6d4203e52a9865d77adb1f5bbb4e29c';
     // const countryName = 'Germany'; // Replace with the desired country name
 
     //cidades api
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
+    // const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
 
     axios.get(apiUrl)
         .then(response => {
