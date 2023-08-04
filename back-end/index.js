aa51573612846517900e34ed08a39d582913dea3
JosephMboane <31317713+JosephMboane@users.noreply.github.com>
2023-08-04 13:06:32 +0200

    ALterations

--- back-end/index.js
+++ back-end/index.js
@@ -1,46 +1,58 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const PORT = 3000;
+const corsOption = {
+
+    origin: "*",
+    methods: ['GET', 'POST']
+}
+const cors = require('cors');
 
 // JSON parser middleware
 app.use(express.json());
-
-// API endpoint for country information
-app.get('/country', (req, res) => {
-    // const countryName = req.body.countryName;
-
-    // // Replace this with your logic to get country information
-    // // For demonstration purposes, we'll use a dummy response
-    // const countryInfo = {
-    //     name: countryName,
-    //     population: 'Dummy population data',
-    //     capital: 'Dummy capital data',
-    //     // Add more information as needed
-    // };
-
-    // res.json(countryInfo);
-    // console.log(countryInfo);
-
-    const axios = require('axios');
+app.use(bodyParser.json());
+app.use(cors(corsOption))
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+});
 
 
-    //temperatura api
-    const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
+// API endpoint for city information
+app.post('/country', async (req, res) => {
+    const requestCity = req.body.city;
+    let city = {}
+    //This variable receives body introduced by the front-end puts in the parameters cities in the api
+    const apiUrlWeather = `https://api.openweathermap.org/data/2.5/weather?q=${requestCity}&appid=41677c1cc1422d4750faf03753b0dbaa`;
+    axios.get(apiUrlWeather)
 
-    //Cambios api
-    // const apiUrl = 'http://api.exchangeratesapi.io/v1/symbols?access_key=f6d4203e52a9865d77adb1f5bbb4e29c';
-    // const countryName = 'Germany'; // Replace with the desired country name
-
-    //cidades api
-    // const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=-26.332807&lon=33.105769&appid=41677c1cc1422d4750faf03753b0dbaa';
-
-    axios.get(apiUrl)
-        .then(response => {
+        .then(async response => {
             console.log('Country Information:', response.data);
+            const responseCoins = await axios.get(`https://restcountries.com/v2/alpha/${response.data.sys.country}`);
+            console.log('moeda', responseCoins.data.currencies[0].code);
+            const populationUrl = await axios.get(`https://api.worldbank.org/v2/country/${response.data.sys.country}/indicator/SP.POP.TOTL?format=json`);
+            const gpdPer = await axios.get(`https://api.worldbank.org/v2/country/${response.data.sys.country}/indicator/NY.GDP.PCAP.CD?format=json`);
+            // console.log('population total', populationUrl.data[1][0].value);
+            // console.log('population total', gpdPer.data[1][0].value);
+            
+            //This object is the response that gives whenever something is introduced into the body
+            city = {
+                name: response.data.name,
+                temp: response.data.main.temp,
+                coin: responseCoins.data.currencies[0].code,
+                population: populationUrl.data[1][0].value,
+                gpd: gpdPer.data[1][0].value
+            }
+            console.log('city', city)
+            res.json(city);
         })
         .catch(error => {
             console.error('Error:', error.message);
         });
+
+
 
 });
 
